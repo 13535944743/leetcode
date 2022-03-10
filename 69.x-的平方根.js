f0ea8bf6b890cc5580751f0fb58f20692d65e04e
clz <1926389897@qq.com>
2022-03-10 23:01:21 +0800

    69.x的平方根

--- 69.x-的平方根.js
+++ 69.x-的平方根.js
@@ -17,10 +17,10 @@ var mySqrt = function (x) {
   while (l <= r) {
     mid = Math.floor((l + r) / 2)
 
-    if (mid * mid <= x) {
-      l = mid + 1
-    } else {
+    if (mid * mid > x) {
       r = mid - 1
+    } else {
+      l = mid + 1
     }
   }
 
