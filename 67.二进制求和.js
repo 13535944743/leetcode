2ec91c2d028f4facb88b593e969c31b94d351044
clz <1926389897@qq.com>
2022-03-10 22:41:04 +0800

    67.二进制求和

--- 67.二进制求和.js
+++ 67.二进制求和.js
@@ -12,26 +12,25 @@
  */
 var addBinary = function (a, b) {
   let str = []
-  let flag = false    // true表示有进位
-  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
+  let flag = 0    // 1表示有进位，0代表没有进位
+  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {   // 只有两个字符串都遍历完，才退出循环
     let sum
-    if (i < 0) {
-      sum = flag ? (b[j] - 0) + 1 : (b[j] - 0)
-      flag = sum >= 2 ? true : false
-    } else if (j < 0) {
-      sum = flag ? (a[i] - 0) + 1 : (a[i] - 0)
-      flag = sum >= 2 ? true : false
-    } else {
-      sum = flag ? (b[j] - 0) + (a[i] - 0) + 1 : (b[j] - 0) + (a[i] - 0)
-      flag = sum >= 2 ? true : false
+
+    if (i < 0) {                // a串遍历完了，但b串没完
+      sum = (b[j] - 0) + flag  // 一开始的-0是把字符转换成number
+    } else if (j < 0) {       // b串遍历完，但a串没完
+      sum = (a[i] - 0) + flag
+    } else {                  // a、b串都没遍历完
+      sum = (b[j] - 0) + (a[i] - 0) + flag
     }
 
+    flag = sum >= 2 ? 1 : 0      // 记录有无进位，用于下一轮
     sum = sum >= 2 ? sum - 2 : sum
-    str.unshift(sum.toString())
+    str.unshift(sum.toString())     // 把这一轮的结果存好
   }
 
   if (flag) {
-    str.unshift(1)
+    str.unshift(1)      // 最后还有进位
   }
 
   return str.join('')
