a2cfa5be07a70ebe82e2b7eee1c79108a24eb1a6
clz <1926389897@qq.com>
2022-03-10 22:29:35 +0800

    66.加一

--- 66.加一.js
+++ 66.加一.js
@@ -13,14 +13,14 @@ var plusOne = function (digits) {
   let n = digits.length - 1
   while (true) {
     let result = digits[n] + 1
-    digits[n--] = result >= 10 ? result - 10 : result
+    digits[n--] = result >= 10 ? result - 10 : result   // 如果+1后，结果大于10，则需要-10
 
-    if (result >= 10 && n < 0) {
+    if (result >= 10 && n < 0) {   // 有进位，且已经遍历结束，需要在数组首部添加进位1
       digits.unshift(1)
       break
     }
 
-    if (result < 10) {
+    if (result < 10) {    // 无进位，直接退出遍历
       break
     }
   }
