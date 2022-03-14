081ae2ea35c59988f288b02b385606592d7c0f9e
clz <1926389897@qq.com>
2022-03-14 11:21:57 +0800

    119.杨辉三角

--- 119.杨辉三角-ii.js
+++ 119.杨辉三角-ii.js
@@ -10,7 +10,7 @@
  * @return {number[]}
  */
 var getRow = function (rowIndex) {
-  let row = new Array(rowIndex + 1).fill(0)
+  let row = new Array(rowIndex + 1).fill(0)   // 索引为rowIndex的那一行有rowIndex+1个元素
 
   row[0] = 1
 
@@ -18,7 +18,6 @@ var getRow = function (rowIndex) {
     for (let j = i; j > 0; j--) {// 从右边往前算，因为一开始就给够了数组长度，没有值的部分都设为0了
       row[j] += row[j - 1]
     }
-    console.log(row)
   }
 
   return row
