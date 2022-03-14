a0ce0c86c5abeb4941a168e506003c8f9358dbf4
clz <1926389897@qq.com>
2022-03-14 11:22:14 +0800

    206.反转链表(迭代)

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -16,8 +16,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
+var reverseList = function (head) {
+  let pre = null              // 每次都存上一个指针
+  let cur = head
 
+  while (cur) {
+    const next = cur.next   // 暂存next指针
+    cur.next = pre          // 当前指针指向前一个指针
+
+    pre = cur               // 继续遍历下去
+    cur = next
+  }
+
+  return pre
 };
 // @lc code=end
 
