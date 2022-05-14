ff60cd116989a49ebe9bc331a4e9c90be43a093b
clz <1926389897@qq.com>
2022-05-14 23:57:59 +0800

    clz: 53.最大子数组和

--- 53.最大子数组和.js
+++ 53.最大子数组和.js
@@ -9,8 +9,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
+  let sum = nums[0]
+  let result = nums[0]
 
+  for (let i = 1; i < nums.length; i++) {
+
+    const x = nums[i]
+
+    sum = Math.max(sum + x, x)
+    result = Math.max(result, sum)
+  }
+
+  return result
 };
 // @lc code=end
 
