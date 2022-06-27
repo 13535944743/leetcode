3466e39742e18ac372642411c06a28b959d9db53
clz <1926389897@qq.com>
2022-06-27 21:50:05 +0800

    102.二叉树的层序遍历

---
+++ 102.二叉树的层序遍历.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  const result = [];
+
+  if (!root) {
+    return result;
+  }
+
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const arr = [];
+    let len = queue.length;
+
+    while (len--) {
+      let node = queue.shift();
+      if (node) {
+        arr.push(node.val);
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    result.push(arr);
+  }
+
+  return result;
+};
+// @lc code=end
+
