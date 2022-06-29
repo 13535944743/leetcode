9a0b5b89161673b62e38b074ce814ed2bd6841f9
clz <1926389897@qq.com>
2022-06-29 22:52:31 +0800

    33.搜索旋转排序数组

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      // [left, mid-1]有序
+
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // [mid, right]有序
+      if (target >= nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+
+  return -1;
+};
+// @lc code=end
+
